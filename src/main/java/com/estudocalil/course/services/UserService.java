aed4b7862029dbdd385b1115524b0e2c854b2201
calilDugue <calildugue@hotmail.com>
2025-03-24 18:07:23 -0300

    Implementando exceção e incremento com PG

--- src/main/java/com/estudocalil/course/services/UserService.java
+++ src/main/java/com/estudocalil/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.estudocalil.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -11,6 +13,9 @@ import org.springframework.stereotype.Service;
 import com.estudocalil.course.repositories.UserRepository;
 import com.estudocalil.course.services.exceptions.DatabaseException;
 import com.estudocalil.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import com.estudocalil.course.entities.User;
 
 @Service
@@ -42,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
