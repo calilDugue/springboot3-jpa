f9c2da80f25be9bc17ce186a734eb04052860584
calilDugue <calildugue@hotmail.com>
2025-03-24 16:55:51 -0300

    Add exceptions

--- src/main/java/com/estudocalil/course/resources/UserResource.java
+++ src/main/java/com/estudocalil/course/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.estudocalil.course.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
